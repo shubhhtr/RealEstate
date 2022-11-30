c19eb79a44527913c3ed5f63b33b262c89897ec5
Shubham Tripathi <shubhhtr@gmail.com>
2022-11-30 18:48:56 +0530

    check

---
+++ src/Components/filtering/Price.js
@@ -0,0 +1,20 @@
+import React,{useState} from 'react'
+
+function PriceRange() {
+  
+
+
+  return (
+    <div className="dropdown">
+<select className="btn">
+  <option className="">All Price</option>
+  <option className=""> 10 Lakhs</option>
+  <option className="">50 Lakhs</option>
+  <option className="">100 Lakhs</option>
+  <option className="">1 Crore</option>
+</select>
+</div>
+  )
+}
+
+export default PriceRange
